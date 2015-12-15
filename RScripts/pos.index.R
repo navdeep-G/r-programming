d5e5cafd6f6622aafb870526db67b49fb8fe6225
Navdeep Gill <mr.navdeepgill@gmail.com>
2015-12-15 12:05:26 -0800

    Update pos.index.R

--- RScripts/pos.index.R
+++ RScripts/pos.index.R
@@ -1,3 +1,5 @@
+#Given a numeric vector x, produce a vector named where.pos, which contains the indices of the positions where the value of x is positive.
+#Example: given x = c(34, -5,0,12,-7,4) you should output a vector named where.pos which has the following entries: 1,4,6.
 pos.index <- function(x)
 {
   if ( !is.vector(x) )  { stop("parameter must be a vector") }
