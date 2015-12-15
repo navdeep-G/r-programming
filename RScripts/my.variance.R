d29d697bf43279f91cc3b17129ed1b10054a28a5
navdeep-G <mr.navdeepgill@gmail.com>
2015-12-15 11:50:54 -0800

    My variance script

---
+++ RScripts/my.variance.R
@@ -0,0 +1,27 @@
+#2
+#Given a numeric vector x, calculate the sample variance using a loop. 
+my.variance <- function(x)
+{
+  if ( !is.vector(x) )  { stop("parameter must be a vector") }
+  if ( !is.numeric(x) ) { stop("parameter must be numeric") }
+  
+  sum=0
+  n = length(x)
+  for(i in 1:n)
+  {
+    sum = sum + x[i]
+    x.bar = sum/n
+  }
+  numerator = numeric(n)
+  for ( i in 1:n)
+  {
+    numerator[i] = (x[i]-x.bar)^2
+  }
+  var = sum(numerator)/(n-1)
+  print(var)
+}
+
+#Test Case
+x<-c(1:20)
+variance(x)
+var(x)
