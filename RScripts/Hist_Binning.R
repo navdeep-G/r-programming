0b55bbe2c42048ae230510b0275ebf1f50b60468
Navdeep Gill <mr.navdeepgill@gmail.com>
2015-12-15 12:06:27 -0800

    Update Hist_Binning.R

--- RScripts/Hist_Binning.R
+++ RScripts/Hist_Binning.R
@@ -1,3 +1,8 @@
+#Input: a numerc data vector called x, and another vector named bounds. The bounds vector is
+#numeric, and strictly monotonic. We want to bin the data in vector x based on bounds. This is what you would do if you are creating a histogram.
+#Output: a vector named bin.count, which contains the count of how many elements of x are in each bin.
+#Example: bounds = c(0,2.5,10). The bins are (-Inf,0),[0,2.5),[2.5,10), and [10,Inf). We return a vector of length 4. The first element of the return vector gives a count of how many x entries are in the interval (-Inf,0), the second element of the return vector gives a count of how many x entries are in the interval [0,2.5), etc. Here “[“ indicates a closed bound and “)” indicates an open bound. Check the input argument for validity. If x = c(-4,10,5,24,12,34), then bin.count should equal c(1,0,1,4)
+#Check that bounds is actually strictly monotonic. If it is not, print an error message.
 hist <- function(x,bounds)
 {
   n = diff(bounds)
